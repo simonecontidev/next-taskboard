e3d532adb7a99f268fbc05d94fa4653bc8a93717
simonecontidev <barriotropical@gmail.com>
2025-10-16 10:21:53 +0200

    install next js, muy and typescript
    Signed-off-by: simonecontidev <barriotropical@gmail.com>

---
+++ components/ToDoContainer.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Box, TextField } from '@mui/material'
+
+const ToDoContainer = () => {
+  return (
+    <div>
+      <Box>
+        <form>
+            <TextField/>
+        </form>
+        </Box>
+    </div>
+  )
+}
+
+export default ToDoContainer
---
+++ src/app/page.tsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+import ToDoContainer from "../../components/ToDoContainer";
+import { Box } from "@mui/material";
+import { Typography } from "@mui/material";
+
+
+const Home = () => {
+  return (
+    <Box>
+    <ToDoContainer/>
+    </Box>
+  );
+}
+
+export default Home;
